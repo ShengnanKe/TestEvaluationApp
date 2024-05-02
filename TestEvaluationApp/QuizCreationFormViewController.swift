8e93e6f09d95daa81375db90da85d1171ea86bb5
KknanxX <72762570+KknanxX@users.noreply.github.com>
2024-05-01 17:01:53 -0700

    changed the initial set up
    messy

---
+++ TestEvaluationApp/QuizCreationFormViewController.swift
@@ -0,0 +1,39 @@
+//
+//  QuizCreationFormViewController.swift
+//  TestEvaluationApp
+//
+//  Created by KKNANXX on 5/1/24.
+//
+
+/*
+ Users can add questions along with options and correct answers through a form within the app. The data will be stored in a text file.
+ Users can access a form to input quiz questions, options, and correct answers.
+ ● Each question must have at least two options and one correct answer.
+ ● Upon submission, the data will be stored in a text file.
+ 
+*/
+
+import UIKit
+
+class QuizCreationFormViewController: UIViewController {
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
